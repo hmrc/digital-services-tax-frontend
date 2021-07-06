09f643e044c505f34256e95df237636a55a1448c
Matt Roberts <mattrobertsky@users.noreply.github.com>
2021-07-06 16:22:23 +0100

    CYA 404 fix

--- app/uk/gov/hmrc/digitalservicestax/aa_data/Period.scala
+++ app/uk/gov/hmrc/digitalservicestax/aa_data/Period.scala
@@ -40,6 +40,7 @@ object Period {
   object Key extends ValidatedType[String]{
     def validateAndTransform(in: String): Option[String] = 
       Some(in).filter{x => x.nonEmpty && x.size <= 4}
+        .map("^#".r.replaceAllIn(_,""))
   }
 
 }
