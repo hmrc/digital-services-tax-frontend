34363bffc1be4e5dc6459fe34ae999d2de2abe43
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2024-02-01 14:16:30 +0000

    DDCYLS-4514:Address pr bot review comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,21 +22,21 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "org.scalatest"                %% "scalatest"                   % "3.2.17",
-    "org.jsoup"                     % "jsoup"                       % "1.15.4",
-    "org.scalacheck"               %% "scalacheck"                  % "1.17.0",
-    "io.chrisdavenport"            %% "cats-scalacheck"             % "0.3.2",
-    "com.beachape"                 %% "enumeratum-scalacheck"       % "1.7.2",
-    "wolfendale"                   %% "scalacheck-gen-regexp"       % "0.1.2",
-    "com.outworkers"               %% "util-samplers"               % "0.57.0",
-    "uk.gov.hmrc.mongo"            %% s"hmrc-mongo-test-play-$play" % hmrcMongoVersion,
-    "uk.gov.hmrc"                  %% s"bootstrap-test-play-$play"  % bootstrapVersion,
-    "com.vladsch.flexmark"          % "flexmark-all"                % "0.64.6",
-    "org.scalatestplus.play"       %% "scalatestplus-play"          % "7.0.0",
-    "org.scalatestplus"            %% "scalacheck-1-17"             % "3.2.17.0",
-    "org.scalatestplus"            %% "mockito-3-12"                % "3.2.10.0",
-    "org.mockito"                   % "mockito-core"                % "5.2.0",
-    "com.luketebbs.uniform"        %% "interpreter-logictable"      % uniformVersion
+    "org.scalatest"          %% "scalatest"                   % "3.2.17",
+    "org.jsoup"               % "jsoup"                       % "1.15.4",
+    "org.scalacheck"         %% "scalacheck"                  % "1.17.0",
+    "io.chrisdavenport"      %% "cats-scalacheck"             % "0.3.2",
+    "com.beachape"           %% "enumeratum-scalacheck"       % "1.7.2",
+    "wolfendale"             %% "scalacheck-gen-regexp"       % "0.1.2",
+    "com.outworkers"         %% "util-samplers"               % "0.57.0",
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-play-$play" % hmrcMongoVersion,
+    "uk.gov.hmrc"            %% s"bootstrap-test-play-$play"  % bootstrapVersion,
+    "com.vladsch.flexmark"    % "flexmark-all"                % "0.64.6",
+    "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.17.0",
+    "org.scalatestplus"      %% "mockito-3-12"                % "3.2.10.0",
+    "org.mockito"             % "mockito-core"                % "5.2.0",
+    "com.luketebbs.uniform"  %% "interpreter-logictable"      % uniformVersion
   ).map(_ % Test)
 
 }
